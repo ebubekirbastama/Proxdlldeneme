c4d695338dc415c24f08288bb12be639e83e505e
ebubekirbastama <ebubekirbastama@elmalicesmekuruyemis.com>
2020-01-17 18:00:39 +0200

    Add project files.

---
+++ Proxdlldeneme/Form1.cs
@@ -0,0 +1,19 @@
+﻿using SeleniumProxyConnect;
+using System;
+using System.Windows.Forms;
+namespace Proxdlldeneme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var dr = BastamaProxy.Proxyconneciton("", textBox1.Text);
+            dr.FindElementByXPath("//span[contains(text(),'ra Bulucu')]").Click();
+        }
+    }
+}
